b1d8994ac68b3a71c8415282cb1e034268d69334
gksaksdma1 <gksaksdma1@naver.com>
2017-09-11 00:05:43 +0900

    Initial commit

---
+++ app/src/main/java/com/example/joon/registration/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.example.joon.registration;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        final TextView registerButton = (TextView) findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(getApplicationContext(),RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/joon/registration/ValidateRequest.java
@@ -0,0 +1,29 @@
+package com.example.joon.registration;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by JOON on 2017-08-17.
+ */
+
+public class ValidateRequest extends StringRequest {
+
+    final static private String URL ="http://gksaksdma1.dothome.co.kr/UserValidate.php";
+    private Map<String, String> parameters;
+
+    public ValidateRequest(String userID, Response.Listener<String> listener) {
+        super(Method.POST,URL,listener,null);
+        parameters = new HashMap<>();
+        parameters.put("userID",userID);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return parameters;
+    }
+
+}
